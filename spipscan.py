27595c6fcaa7a6f48ef94df02c1c868b5e6fe604
Paul AMAR <aos.paul@gmail.com>
2014-01-09 21:21:54 +0100

    Added a new detection for paquet.xml (new gen) plugins

--- spipscan.py
+++ spipscan.py
@@ -29,20 +29,30 @@ def detect_plugins(url):
         links_to_plugins = soup('a')
         for link in links_to_plugins:
             # grabbing the folder of the plugin
+            regex_plugin = re.search(r"href=\"(\w+/)\"> (\w+)/<", str(link))
             try:
-                regex_plugin = re.search(r"href=\"(\w+/)\"> (\w+)/<", str(link))
                 url_plugin = url + regex_plugin.group(1) + "plugin.xml"
                 name_plugin = regex_plugin.group(2)
                 # HTTP GET to get the version of the plugin
                 req_plugin_xml = requests.get(url_plugin, timeout=5)
                 regex_version_plugin = re.search(r"<version>(\d+(.\d+)?(.\d+)?)</version>", req_plugin_xml.content)
                 print "[!] Plugin " + str(name_plugin) + " detected. Version : " + str(regex_version_plugin.group(1))
+                print "URL : " + url_plugin
             except:
-                pass
+                try:
+                    url_plugin = url + regex_plugin.group(1) + "paquet.xml"
+                    name_plugin = regex_plugin.group(2)
+                    # HTTP GET to get the version of the plugin
+                    req_plugin_xml = requests.get(url_plugin, timeout=5)
+                    regex_version_plugin = re.search(r"version=\"(\d+(.\d+)?(.\d+)?)\"", req_plugin_xml.content)
+                    print "[!] Plugin " + str(name_plugin) + " detected. Version : " + str(regex_version_plugin.group(1))
+                    print "URL : " + url_plugin
+                except:
+                    pass
 
     elif (req.status_code == 403):
         # folder might exist but not accessible
-        # gonna try to detect plugins
+        # gonna try to detect plugins with brute force attack
         print "[-] folder plugins/ is forbidden"
     elif (req.status_code == 404):
         # folder seems to not exist
